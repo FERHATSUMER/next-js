6b5b862d00ae2c5583945b6eeede2f60a0c015ff
FERHATSUMER <sumerf39@gmail.com>
2023-04-10 01:42:52 +0300

    next.js

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import headerStyles from "../styles/header.module.css"
+const Header = () => {
+  return (
+    <div>
+      <h1 className={headerStyles.title}>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
